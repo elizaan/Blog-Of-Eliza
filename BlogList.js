87a932148fcc80e238d65b352e85ded63ae7f0b7
Ishrat Eliza <37586198+elizaan@users.noreply.github.com>
2021-04-29 01:38:48 +0600

    new blog adding form added and deletion of blog added

--- BlogList.js
+++ BlogList.js
@@ -14,13 +14,13 @@ const BlogList = (props) => {
         <div className="blog-previews" key={blog.id} >
           <Link to ={`/blogs/${blog.id}`}>
             <h2>{blog.title}</h2>
-              <div className="body"> 
+              {/* <div className="body"> 
                 <p class="b">{blog.body}</p>
-              </div>
+              </div> */}
               <div className="author">
                 <p class="author" fontSize = "6">written by - {blog.author}</p>
               </div>
-              <button onClick = {()=> handleDelete(blog.id)}>Delete blog</button>
+              {/* <button onClick = {()=> handleDelete(blog.id)}>Delete blog</button> */}
 
           </Link>
           
